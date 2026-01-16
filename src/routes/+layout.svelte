02fcdcc1b2b05ac8e1ff04fce7b1a66fad11c486
michaelchristwin <kelechukwuchristwin@gmail.com>
2026-01-16 14:38:23 +0100

    Chore: Added reviews

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,9 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-<header class="sticky top-0 z-10 flex h-12.5 w-full items-center justify-between bg-white px-20">
+<header
+	class="sticky top-0 z-20 flex h-12.5 w-full items-center justify-between bg-[#3f3e73] px-20 text-white"
+>
 	<h1 class="text-[16px] font-bold md:text-lg">ALLIANCE POWER</h1>
 	<nav>
 		<ul class="flex h-full items-center justify-around space-x-4 text-sm font-bold md:text-[16px]">
